587ef5c1c0a678dca20b33b327c62184229c99e7
allemx <allemx@gmail.com>
2022-09-14 10:15:58 -0300

    Primeiro commit

---
+++ fatorial.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 21; i> 0;i--{
+	res := Fatorial(i)
+	fmt.Println("Res : ", i , res)}
+	fmt.Println
+}
+func Fatorial(x int) int {
+	var y  = 1
+	for ; x > 0; x-- {
+		y = y * x
+	}
+	return y
+}
