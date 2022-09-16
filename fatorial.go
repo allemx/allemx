097d8e05a5a3ccf67730b311adb4a08c340793e2
allemx <allemx@gmail.com>
2022-09-15 22:17:11 -0300

    OK

--- fatorial.go
+++ fatorial.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-	for i := 21; i> 0;i--{
-	res := Fatorial(i)
-	fmt.Println("Res : ", i , res)}
-	
-=======
 	var i float64
 	for i = 171; i > 0; i-- {
 		res := Fatorial(i)
 		fmt.Println("Res : ", i, res)
 	}
 
->>>>>>> fatorial
 }
 func Fatorial(x float64) float64 {
 	var y float64 = 1
